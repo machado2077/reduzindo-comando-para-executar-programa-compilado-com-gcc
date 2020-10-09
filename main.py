0161c08c205972a0888f020c04b85ac169354681
Matheus <machadom@outlook.com.br>
2020-10-09 20:23:00 -0300

    permitindo a compilação em outros diretórios

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import sys
 import os
 
+if len(sys.argv) == 1 or \
+	not os.path.exists(os.path.join(os.getcwd(), sys.argv[1])):
+	print('File not given or nonexistent.')
+	sys.exit()
 code = sys.argv[1]
 compiled = f'{code[:-2]}_compiled'
-if sys.argv[2]:
+if len(sys.argv) > 2:
 	compiled = sys.argv[2]
 os.system(f'gcc {code} -o {compiled} && ./{compiled}')
