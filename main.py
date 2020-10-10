cd949926c6705804dc55efed8d2a47308a3d48ec
Matheus <machadom@outlook.com.br>
2020-10-09 23:06:29 -0300

    generalizando a junção de arquivos e o apontamento do executavel

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import sys
 import os
 
+def path_join(file_name: str) -> str:
+	return os.path.join(os.getcwd(), file_name)
+
 if len(sys.argv) == 1 or \
-	not os.path.exists(os.path.join(os.getcwd(), sys.argv[1])):
+	not os.path.exists(path_join(sys.argv[1])):
 	print('File not given or nonexistent.')
 	sys.exit()
 code = sys.argv[1]
-compiled = f'{code[:-2]}[compiled]'
+compiled = path_join(f'{code[:-2]}[compiled]')
 if len(sys.argv) > 2:
-	compiled = sys.argv[2]
+	compiled = path_join(sys.argv[2])
 os.system(f'gcc {code} -o {compiled} && {compiled}')
