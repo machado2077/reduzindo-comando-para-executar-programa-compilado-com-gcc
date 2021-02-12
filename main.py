53ca8b8210300dc54c5c308d521d40c0d3971fc1
Matheus <machadom@outlook.com.br>
2021-02-12 13:26:18 -0300

    melhorado a abrangencia para código cpp

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+"""
+expected command:
+
+$ alias file_name [compiled_name] [permanent_compiled_file]
+"""
+
 import sys
 import os
+
 
 def path_join(file_name: str) -> str:
 	return os.path.join(os.getcwd(), file_name)
@@ -10,16 +17,28 @@ def main():
 		not os.path.exists(path_join(sys.argv[1])):
 		print('File not given or nonexistent.')
 		return
-	code = sys.argv[1]
-	compiled = path_join(f'{code[:-2]}__COMPILED')
-	if len(sys.argv) > 2:
+	file_name = sys.argv[1]
+
+	if file_name.endswith('.c'):
+		compiled = path_join(f'{file_name[:-2]}__COMPILED')
+		driver = 'gcc'
+	elif file_name.endswith('.cpp'):
+		compiled = path_join(f'{file_name[:-4]}__COMPILED')
+		driver = 'g++'
+	else:
+		print("Unknown source code extension.")
+		return
+	
+	if len(sys.argv) > 3:
 		compiled = path_join(sys.argv[2])
-	os.system(f'g++ {code} -o {compiled}')
+	os.system(f'{driver} {file_name} -o {compiled}')
+	
 	if os.path.exists(path_join(compiled)):
 		os.system(f'{compiled}')
 	else:
-		print("Compilation error.")
-	if sys.argv[-1] != '-p':
+		return
+	
+	if not '-p' in sys.argv:
 		os.remove(compiled)
 
 
